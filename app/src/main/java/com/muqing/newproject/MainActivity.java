950e0f69c74dc5b6c80fa7babde056d2295ae292
tzbin <503105456@qq.com>
2020-07-02 15:17:24 +0800

    第一次提交

--- app/src/main/java/com/muqing/newproject/MainActivity.java
+++ app/src/main/java/com/muqing/newproject/MainActivity.java
@@ -1,15 +1,29 @@
 package com.muqing.newproject;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        FrameLayout fl = findViewById(R.id.fl);
+        TextView textView = new TextView(this);
+        textView.setTextColor(getResources().getColor(R.color.colorAccent));
+        textView.setTextSize(18);
+        textView.setText("HelloWorld");
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.gravity = Gravity.CENTER;
+        textView.setLayoutParams(lp);
+        fl.addView(textView);
     }
 }
